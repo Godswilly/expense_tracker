ddfede9dc4eb8cdbb8e9a99b26bd8aeec91b60f9
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2020-11-11 10:46:17 +0100

    Created home controller, view and set the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :groups, only: %I[index new create show]
+  resources :expenses, only: %I[index new create]
+  resources :users, only: [:create]
+  root 'home#index'
 end
