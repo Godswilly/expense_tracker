7024abc0d2fb2a24acec1a63cedfd17d1f6f89e2
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2020-11-14 11:32:02 +0100

    fixed association errors in the models

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,9 +1,9 @@
 class Expense < ApplicationRecord
-  belongs_to :User
+  belongs_to :user
   has_many :expense_groups
   has_many :groups, through: :expense_groups
   validates :name, presence: true, length: { minimum: 3, maximun: 25 }
   validates_numericality_of :amount, greater_than: 0, allow_nil: true
 
-  scope :desc, -> { includes(:author).order('created_at DESC') }
+  scope :desc, -> { includes(:user).order('created_at DESC') }
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :user
 
   has_many :expense_groups
 
