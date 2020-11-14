bef5d0b42aa7f73f90f0439b4f332949e705c141
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2020-11-14 11:15:42 +0100

    updated expenses_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      session[:author_id] = @user.id
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Group-It #{@user.username}"
       redirect_to root_path
     else
